88d9fb6ea48d9b4962a4d5370b97aeae05ccb245
Henrique <hgqn.snf20@uea.edu.br>
2021-05-20 23:40:35 -0300

    paddle bug fix and multiple angle hits on paddle

--- Activity #04/game.py
+++ Activity #04/game.py
@@ -29,18 +29,25 @@ scoring_sound_effect = pygame.mixer.Sound('assets/bleep.wav')
 
 # player 1
 player_1 = pygame.image.load("assets/player.png")
+rect_player_1 = player_1.get_rect()
 player_1_y = 300
+rect_player_1 = rect_player_1.move(50, player_1_y)
 player_1_move_up = False
 player_1_move_down = False
 
+
 # player 2 - robot
 player_2 = pygame.image.load("assets/player.png")
+rect_player_2 = player_2.get_rect()
 player_2_y = 300
+rect_player_2 = rect_player_2.move(1180, player_2_y)
 
 # ball
 ball = pygame.image.load("assets/ball.png")
+rect_ball_ = ball.get_rect()
 ball_x = 640
 ball_y = 360
+rect_ball = rect_ball_.move(ball_x, ball_y)
 ball_dx = 5
 ball_dy = 5
 
@@ -77,81 +84,102 @@ while game_loop:
         screen.fill(COLOR_BLACK)
 
         # ball collision with the wall
-        if ball_y > 700:
+        if rect_ball.y > 700:
             ball_dy *= -1
             bounce_sound_effect.play()
-        elif ball_y <= 0:
+        elif rect_ball.y <= 0:
             ball_dy *= -1
             bounce_sound_effect.play()
 
         # ball collision with the player 1 's paddle
-        if ball_x < 100:
-            if player_1_y < ball_y + 25:
-                if player_1_y + 150 > ball_y:
-                    ball_dx *= -1
-                    bounce_sound_effect.play()
+        if rect_ball.colliderect(rect_player_1) and ball_dx < 0:
+            ball_dx *= -1
+            if rect_player_1.bottom >= rect_ball.top > rect_player_1.centery + 30:
+                if ball_dy == 0:
+                    ball_dy = 5
+                else:
+                    ball_dy *= 1
+
+            elif rect_player_1.top <= rect_ball.bottom < rect_player_1.centery - 30:
+                if ball_dy == 0:
+                    ball_dy = 5
+                else:
+                    ball_dy *= -1
+                bounce_sound_effect.play()
+            elif rect_player_1.centery + 30 >= rect_ball.centery > rect_player_1.centery - 30:
+                ball_dy = 0
 
         # ball collision with the player 2 's paddle
-        if ball_x > 1160:
-            if player_2_y < ball_y + 25:
-                if player_2_y + 150 > ball_y:
-                    ball_dx *= -1
-                    bounce_sound_effect.play()
+        elif rect_ball.colliderect(rect_player_2) and ball_dx > 0:
+            ball_dx *= -1
+            if rect_player_2.bottom >= rect_ball.top > rect_player_2.centery + 30:
+                if ball_dy == 0:
+                    ball_dy = -5
+                else:
+                    ball_dy *= 1
+            elif rect_player_2.top <= rect_ball.bottom < rect_player_2.centery - 30:
+                if ball_dy == 0:
+                    ball_dy = -5
+                else:
+                    ball_dy *= -1
+                bounce_sound_effect.play()
+            elif rect_player_2.centery + 30 >= rect_ball.centery > rect_player_2.centery - 30:
+                ball_dy = 0
 
         # scoring points
-        if ball_x < -50:
-            ball_x = 640
-            ball_y = 360
+        if rect_ball.x < -50:
+            rect_ball.x = 640
+            rect_ball.y = 360
             ball_dy *= -1
             ball_dx *= -1
             score_2 += 1
             scoring_sound_effect.play()
-        elif ball_x > 1320:
-            ball_x = 640
-            ball_y = 360
+        elif rect_ball.x > 1320:
+            rect_ball.x = 640
+            rect_ball.y = 360
             ball_dy *= -1
             ball_dx *= -1
             score_1 += 1
             scoring_sound_effect.play()
 
         # ball movement
-        ball_x = ball_x + ball_dx
-        ball_y = ball_y + ball_dy
+        rect_ball.x = rect_ball.x + ball_dx
+        rect_ball.y = rect_ball.y + ball_dy
 
         # player 1 up movement
         if player_1_move_up:
-            player_1_y -= 5
+            rect_player_1.y -= 5
         else:
-            player_1_y += 0
+            rect_player_1.y += 0
 
         # player 1 down movement
         if player_1_move_down:
-            player_1_y += 5
+            rect_player_1.y += 5
         else:
-            player_1_y += 0
+            rect_player_1.y += 0
 
         # player 1 collides with upper wall
-        if player_1_y <= 0:
-            player_1_y = 0
+        if rect_player_1.y <= 0:
+            rect_player_1.y = 0
 
         # player 1 collides with lower wall
-        elif player_1_y >= 570:
-            player_1_y = 570
+        elif rect_player_1.y >= 570:
+            rect_player_1.y = 570
 
         # player 2 "Artificial Intelligence"
-        player_2_y = ball_y
-        if player_2_y <= 0:
-            player_2_y = 0
-        elif player_2_y >= 570:
-            player_2_y = 570
+        rect_player_2.y = rect_ball.y
+        if rect_player_2.y <= 0:
+            rect_player_2.y = 0
+        elif rect_player_2.y >= 570:
+            rect_player_2.y = 570
 
         # update score hud
         score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
 
         # drawing objects
-        screen.blit(ball, (ball_x, ball_y))
-        screen.blit(player_1, (50, player_1_y))
-        screen.blit(player_2, (1180, player_2_y))
+        screen.blit(ball, (rect_ball.x, rect_ball.y))
+        screen.blit(player_1, (rect_player_1.x, rect_player_1.y))
+        screen.blit(player_2, (rect_player_2.x, rect_player_2.y))
         screen.blit(score_text, score_text_rect)
     else:
         # drawing victory
